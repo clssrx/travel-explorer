df41a3157e86c4f5c71af5eab8722cc4466f3095
clssrx <leon.colosio@icloud.com>
2026-04-20 11:42:12 +0200

    setup django experience model, admin and seed data

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,16 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'experiences',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
--- backend/experiences/admin.py
+++ backend/experiences/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Experience
 
-# Register your models here.
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'location', 'category', 'price', 'rating')
+    search_fields = ('title', 'location', 'category')
+    list_filter = ('category',)
