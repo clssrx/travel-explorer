d2761fc869e5b2f0a9bc71dc54670ddb4d88c1d7
clssrx <leon.colosio@icloud.com>
2026-05-12 18:20:43 +0200

    add django rest api for experience list and detail views

--- backend/experiences/views.py
+++ backend/experiences/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Experience
+from .serializer import ExperienceSerializer
+
+
+class ExperienceListView(APIView):
+    def get(self, request):
+        experiences = Experience.objects.all().order_by('id')
+        serializer = ExperienceSerializer(experiences, many=True)
+        return Response(serializer.data)
+
+
+class ExperienceDetailView(APIView):
+    def get(self, request, pk):
+        experience = get_object_or_404(Experience, pk=pk)
+        serializer = ExperienceSerializer(experience)
+        return Response(serializer.data)
